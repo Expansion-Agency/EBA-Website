1dfc7021c5cb9b45856a306c3e1bab8a7a059d6a
Hajar-Medhat <hagarmehat2001@gmail.com>
2025-09-07 11:56:30 +0300

    payment

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,14 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';      // <-- Import AuthModule
-
+import { PaymentModule } from './payment/payment.module';  // <-- Import PaymentModule
+import { PaymentController } from './payment/payment.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),        // <-- Add ConfigModule for env vars
     UsersModule,
-    AuthModule,                                       // <-- Add AuthModule here
+    AuthModule,
+    PaymentModule,                               // <-- Add AuthModule here
   ],
-  controllers: [AppController],
+  controllers: [AppController,PaymentController],
   providers: [AppService],
 })
 export class AppModule {}
